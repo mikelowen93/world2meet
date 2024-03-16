40bc5ea2169ca11a86c1d0ea1fdfbddd3a9642b3
DESKTOP-VDR4JDB\Miguel <migueljerez_11@hotmail.com>
2024-03-16 09:41:19 +0100

    CHORE: it works, styles WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -13,6 +14,7 @@ import { AppComponent } from './app.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroEditComponent } from './hero-edit/hero-edit.component';
 import { HeroCreateComponent } from './hero-create/hero-create.component';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { HeroCreateComponent } from './hero-create/hero-create.component';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+    MatSnackBarModule,
     MatButtonModule,
     MatInputModule,
     MatCardModule,
